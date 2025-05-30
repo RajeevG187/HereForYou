cf32da8766cbe386eca3a1f41ef40081ce561beb
RajeevG187 <rajeevg@iitbhilai.ac.in>
2025-05-30 15:25:57 +0530

    fixed admin routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -28,14 +28,13 @@ const App = () => {
         <SideBar />
         <Routes>
           {/* Admin ROutes */}
-        <Route path='/' element = {<></>} />
-        <Route path='/admin-dashboard' element = {<Dashboard/>} />
+        <Route path='/' element = {<Dashboard/>} />
         <Route path='/all-appointments' element = {<AllAppointments/>} />
         <Route path='/add-doctor' element = {<AddDoctor/>} />
         <Route path='/doctors-list' element = {<DoctorsList/>} />
 
         {/* Doctor Routes */}
-        <Route path='/doctor-dashboard' element = {<DoctorDashboard/>} />
+        <Route path='/' element = {<DoctorDashboard/>} />
         <Route path='/doctor-appointments' element = {<DoctorAppointment/>} />
         <Route path='/doctor-profile' element = {<DoctorProfile/>} />
         </Routes>
