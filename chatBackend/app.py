0ae462e8583f2e7511ec98ed721b9a995c404abd
RajeevG187 <rajeevg@iitbhilai.ac.in>
2025-05-30 06:22:19 +0530

    Fixed : Chat app connection

--- chatBackend/app.py
+++ chatBackend/app.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("FRONTEND_URL")],
+    allow_origins=[os.getenv("FRONTEND_URL"), "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
